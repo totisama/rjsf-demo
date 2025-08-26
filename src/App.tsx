f88923f608b3c582e0dbdf3de4b1e04eafa81c1c
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-08-26 13:25:24 +0200

    render second form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // import { TodoForm } from './components/forms/todo-form'
-import { FirstForm } from './components/forms/first-form'
+// import { FirstForm } from './components/forms/first-form'
 import './App.css'
+import { SecondForm } from './components/forms/second-form'
 
 function App() {
   // return <TodoForm />
-  return <FirstForm />
+  // return <FirstForm />
+  return <SecondForm />
 }
 
 export default App
