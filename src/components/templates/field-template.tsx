eebc36dadb64132d61bc9921783e4a4021d8fc88
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-08-26 13:25:36 +0200

    condition to display label

--- src/components/templates/field-template.tsx
+++ src/components/templates/field-template.tsx
@@ -1,11 +1,11 @@
 import type { FieldTemplateProps } from '@rjsf/utils'
 
 export const FieldTemplate = (props: FieldTemplateProps) => {
-  const { id, label, required, children, rawErrors = [] } = props
+  const { id, label, required, children, rawErrors = [], displayLabel } = props
 
   return (
     <div style={{ marginBottom: 16 }}>
-      {label && (
+      {displayLabel && label && (
         <label
           htmlFor={id}
           style={{ display: 'block', fontWeight: 600, marginBottom: 6 }}
