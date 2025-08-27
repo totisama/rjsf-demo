e278d1c36c89a24ce1184a418d0c968629436afc
Rodrigo Samayoa <toti1998sama@gmail.com>
2025-08-27 12:33:28 +0200

    simple navigation menu to display the three components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-// import { TodoForm } from './components/forms/todo-form'
-// import { FirstForm } from './components/forms/first-form'
-import './App.css'
+import { useState } from 'react'
+import { TodoForm } from './components/forms/todo-form'
+import { FirstForm } from './components/forms/first-form'
 import { SecondForm } from './components/forms/second-form'
+import './App.css'
+
+const forms = {
+  todo: { label: 'Todo Form', component: <TodoForm /> },
+  first: { label: 'First Form', component: <FirstForm /> },
+  second: { label: 'Second Form', component: <SecondForm /> },
+} as const
+
+type FormType = keyof typeof forms
 
 function App() {
-  // return <TodoForm />
-  // return <FirstForm />
-  return <SecondForm />
+  const [activeForm, setActiveForm] = useState<FormType>('todo')
+
+  return (
+    <div>
+      <header className="topbar">
+        <nav className="tabs">
+          {Object.entries(forms).map(([key, { label }]) => {
+            const isActive = key === activeForm
+
+            return (
+              <button
+                key={key}
+                onClick={() => setActiveForm(key as FormType)}
+                className={`tab ${isActive ? 'active' : ''}`}
+              >
+                {label}
+              </button>
+            )
+          })}
+        </nav>
+      </header>
+
+      <main className="content">{forms[activeForm]?.component}</main>
+    </div>
+  )
 }
 
 export default App
